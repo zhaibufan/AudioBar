74b41fa3251cd86231ce359dd99e496731ece482
zhaixiaofan <zhaixiaofan@qq.com>
2017-11-11 16:05:13 +0800

    init

---
+++ app/src/main/java/com/example/zhai/auduiobar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhai.auduiobar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
